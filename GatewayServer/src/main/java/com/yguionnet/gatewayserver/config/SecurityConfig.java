05d4800c96d7b8c02672c2745cd1c7405fc35d40
yguionnet <yannick.guionnet@guiodesv.fr>
2022-11-24 18:09:47 +0100

    security oauth2

---
+++ GatewayServer/src/main/java/com/yguionnet/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.yguionnet.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.pathMatchers("/eazybank/accounts/**").authenticated()
+                        .pathMatchers("/eazybank/cards/**").authenticated()
+                        .pathMatchers("/eazybank/loans/**").permitAll())
+                .oauth2ResourceServer().jwt();
+        http.csrf().disable();
+        return http.build();
+    }
+
+    
+
+}
